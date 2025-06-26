fb294e2a24bc9b4245489fd1c7515ed8babd90f5
HirthikBalaji <hirthikbalaji2006@gmail.com>
2025-06-26 23:06:39 +0530

    Readme

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from transformers import BlipProcessor, BlipForConditionalGeneration
+from PIL import Image
+import requests
+import torch
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def caption_image(image_path_or_url):
+    if image_path_or_url.startswith('http'):
+        image = Image.open(requests.get(image_path_or_url, stream=True).raw).convert('RGB')
+    else:
+        image = Image.open(image_path_or_url).convert('RGB')
+
+    inputs = processor(images=image, return_tensors="pt")
+    with torch.no_grad():
+        out = model.generate(**inputs)
+
+    caption = processor.decode(out[0], skip_special_tokens=True)
+    return caption
+
+
+img_url = "download.jpeg"
+print(caption_image(img_url))
+img = "bird.jpeg"
+print(caption_image(img))
