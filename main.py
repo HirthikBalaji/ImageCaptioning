f146e60ead379614aa931a187da12d5d3f1d27fa
HirthikBalaji <hirthikbalaji2006@gmail.com>
2025-06-28 17:53:59 +0530

    v2

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
+from flask import Flask, request, jsonify
 from transformers import BlipProcessor, BlipForConditionalGeneration
 from PIL import Image
-import requests
 import torch
+import requests
+import os
 
+app = Flask(__name__)
 
+# Load BLIP model
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
-
-def caption_image(image_path_or_url):
-    if image_path_or_url.startswith('http'):
-        image = Image.open(requests.get(image_path_or_url, stream=True).raw).convert('RGB')
-    else:
-        image = Image.open(image_path_or_url).convert('RGB')
-
+def caption_image(image):
     inputs = processor(images=image, return_tensors="pt")
     with torch.no_grad():
         out = model.generate(**inputs)
+    return processor.decode(out[0], skip_special_tokens=True)
 
-    caption = processor.decode(out[0], skip_special_tokens=True)
-    return caption
+@app.route("/caption", methods=["POST"])
+def generate_caption():
+    if 'image' in request.files:
+        image = Image.open(request.files['image']).convert('RGB')
+    elif 'url' in request.json:
+        img_url = request.json['url']
+        image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
+    else:
+        return jsonify({'error': 'No image or URL provided'}), 400
 
+    caption = caption_image(image)
+    return jsonify({'caption': caption})
 
-img_url = "download.jpeg"
-print(caption_image(img_url))
-img = "bird.jpeg"
-print(caption_image(img))
+from flask_cors import CORS
+CORS(app)
